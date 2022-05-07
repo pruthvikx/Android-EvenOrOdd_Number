4c638b36b341c0238bb964b3ab59213cf0dd4b79
Pruthvi Kolhe <65055563+pruthvikx@users.noreply.github.com>
2022-05-07 11:45:45 +0530

    type integer to know it is even or odd

--- app/src/main/java/com/iif/evenorodd/MainActivity.java
+++ app/src/main/java/com/iif/evenorodd/MainActivity.java
@@ -12,19 +12,21 @@ public class MainActivity extends AppCompatActivity {
     public void checkFunction(View view){
 
         EditText editText = (EditText) findViewById(R.id.editText);
-        String stringEditText = editText.getText().toString();
-        int intEditText = Integer.parseInt(stringEditText);
-
         String message;
+        if(editText.getText().toString().isEmpty()){
+            message = "enter a number";
+        }else {
+            String stringEditText = editText.getText().toString();
+            int intEditText = Integer.parseInt(stringEditText);
 
-        if (intEditText < 0){
-            message = "enter valid number";
-        } else if(intEditText % 2 == 0){
-            message = "Even Number";
-        } else {
-            message = "Odd Number";
+            if (intEditText < 0) {
+                message = "enter valid number";
+            } else if (intEditText % 2 == 0) {
+                message = "Even Number";
+            } else {
+                message = "Odd Number";
+            }
         }
-
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 
     }
